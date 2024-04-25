a1c86dec30303c0693963234e61ff256e65360e7
shiqi0515 <shiqi0515@outlook.com>
2024-04-25 11:56:43 +0200

    Add dependencies modify Dashboard

--- js/Components/Layout/Header.jsx
+++ js/Components/Layout/Header.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { FaBars } from "react-icons/fa"; // Import burger menu icon from react-icons
 
 export default function Header() {
-	return (
-		<div id="Header" className="bg-gray-800 text-white p-4 flex justify-between items-center">
-			<div className="text-xl font-bold">Your Application Name</div>
-			<button className="focus:outline-none">
-				<FaBars className="text-xl" />
-			</button>
-		</div>
-	);
+  return (
+    <div
+      id="Header"
+      className="bg-gray-800 text-white p-4 flex justify-between items-center"
+    >
+      <div className="text-xl font-bold">Healthcare in Your Hands</div>
+      <button className="focus:outline-none">
+        <FaBars className="text-xl" />
+      </button>
+    </div>
+  );
 }
--- js/Components/Pages/DashboardPage.jsx
+++ js/Components/Pages/DashboardPage.jsx
@@ -1,27 +1,41 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 import GeneralLayout from "@/js/Components/Layout/GeneralLayout";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
-import {CollectionsContext} from "@/pages";
+import { CollectionsContext } from "@/pages";
 
-export default function DashboardPage(props){
-	const collections = useContext(CollectionsContext);
-	console.log(collections);
+export default function DashboardPage(props) {
+  const collections = useContext(CollectionsContext);
+  console.log(collections);
 
-	return (
-		<GeneralLayout>
-			<div id={'DashboardPage'}>
-				<div>
-					<div className={'content-container'}>
-						<h1 className={'text-5xl text-center mb-20 mt-10'}>
-							Dashboard
-						</h1>
-					</div>
-					<div className={'content-container'}>
-					</div>
-					<div className={'content-container'}>
-					</div>
-				</div>
-			</div>
-		</GeneralLayout>
-	)
+  return (
+    <GeneralLayout>
+      <div id={"DashboardPage"}>
+        <div>
+          <div className={"content-container"}>
+            <h1 className={"text-3xl text-center mb-10 mt-10"}>
+              Quick access Dashboard
+            </h1>
+          </div>
+          <form style={{ display: "flex", justifyContent: "center" }}>
+            <TextField
+              variant="outlined"
+              placeholder="Search..."
+              sx={{ width: "95%" }}
+              InputProps={{
+                endAdornment: (
+                  <IconButton type="submit">
+                    <SearchIcon />
+                  </IconButton>
+                ),
+              }}
+            />
+          </form>
+          <div className={"content-container"}></div>
+        </div>
+      </div>
+    </GeneralLayout>
+  );
 }
