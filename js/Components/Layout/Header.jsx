fc60bb7f0cf42ca0d9bea1658003f8629d30b595
guritaalexandru <a_gurita@yahoo.com>
2024-04-25 12:22:21 +0300

    slight layout fix

--- js/Components/Layout/Header.jsx
+++ js/Components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import { FaBars } from "react-icons/fa"; // Import burger menu icon from react-i
 
 export default function Header() {
 	return (
-		<div id="Header" className="bg-gray-800 text-white p-4 flex justify-between items-center">
+		<div id="Header" className="fixed top-0 left-0 w-full bg-gray-800 text-white p-4 flex justify-between items-center">
 			<div className="text-xl font-bold">Your Application Name</div>
 			<button className="focus:outline-none">
 				<FaBars className="text-xl" />
