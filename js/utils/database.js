fb378b8fc9e90cd06fd03a8f7c9b8336021e9a5f
guritaalexandru <a_gurita@yahoo.com>
2024-04-25 15:07:08 +0300

    eslinting

--- js/utils/database.js
+++ js/utils/database.js
@@ -1,24 +1,24 @@
 import Database from 'better-sqlite3';
-const db = new Database('hercules.db', { verbose: console.log });
+const db = new Database('hercules.db', { verbose: console.log, });
 db.pragma('journal_mode = WAL');
 
 const getAllCollections = () => {
 	return db.prepare('SELECT * FROM COLLECTIONS').all();
-}
+};
 
-const getCollectionById = (id) => {
+const getCollectionById = id => {
 	return db.prepare('SELECT * FROM COLLECTIONS WHERE id = ?').get(id);
-}
+};
 
-const getCollectionBySlug = (slug) => {
+const getCollectionBySlug = slug => {
 	return db.prepare('SELECT * FROM COLLECTIONS WHERE slug = ?').get(slug);
-}
+};
 
-const getArticlesByCollection = (id) => {
+const getArticlesByCollection = id => {
 	return db.prepare('SELECT * FROM ARTICLES WHERE collection_id = ?').all(id);
-}
+};
 
-const getArticlesByCollectionSlug = (slug) => {
+const getArticlesByCollectionSlug = slug => {
 	return db.prepare(`
     SELECT ARTICLES.*
     FROM ARTICLES
@@ -27,18 +27,17 @@ const getArticlesByCollectionSlug = (slug) => {
   `).all(slug);
 };
 
-
 const getAllArticles = () => {
 	return db.prepare('SELECT * FROM ARTICLES').all();
-}
+};
 
-const getArticleById = (id) => {
+const getArticleById = id => {
 	return db.prepare('SELECT * FROM ARTICLES WHERE id = ?').get(id);
-}
+};
 
-const getArticleBySlug = (slug) => {
+const getArticleBySlug = slug => {
 	return db.prepare('SELECT * FROM ARTICLES WHERE slug = ?').get(slug);
-}
+};
 
 export {
 	getAllCollections,
@@ -48,5 +47,5 @@ export {
 	getArticleById,
 	getCollectionBySlug,
 	getArticleBySlug,
-	getArticlesByCollectionSlug
+	getArticlesByCollectionSlug,
 };
